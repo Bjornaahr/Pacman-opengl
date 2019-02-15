c21b1a9d56cdd5e8d5c76b74ea8bc27c2a0540fe
Onhil <onhilx@gmail.com>
2019-02-15 16:34:23 +0100

    Animation class. from lab 4. will likely need some changes

---
+++ src/AnimationController.cpp
@@ -0,0 +1,25 @@
+#include <AnimationController.h>
+
+void AnimationController::setanimation(int animationIndex) {
+	this->currentAnimation = animationIndex;
+}
+
+void AnimationController::step() {
+	if (++currentFrame >= animations[currentAnimation].frameCount)
+	{
+		currentAnimation = animations[currentAnimation].nextAnimation;
+		currentFrame = 0;
+	}
+	int frameIndex = (animations[currentAnimation].startFrame + currentFrame);
+	frame[0] = glm::vec2{
+		(frameIndex % spritesPerSize.x) * (1.0f / spritesPerSize.x),
+		(frameIndex / spritesPerSize.x) * (1.0f / spritesPerSize.x),
+	};
+	frame[1] = frame[0] + glm::vec2{ spriteSize.x, 0.0f };
+	frame[2] = frame[0] + spriteSize;
+	frame[3] = frame[0] + glm::vec2{ 0.0f, spriteSize.y };
+}
+
+const glm::vec2* AnimationController::getFrame() const {
+	return frame;
+}
